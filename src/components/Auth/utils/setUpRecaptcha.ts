c8720b789893ebe737e14d156ca10896d251fc29
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-12-23 12:20:38 +0200

    refactor buttons Auth, refactor auth utils, fix redirect of custom hook useOnAuthStateChanged

--- src/components/Auth/utils/setUpRecaptcha.ts
+++ src/components/Auth/utils/setUpRecaptcha.ts
@@ -1,18 +1,21 @@
-import { Auth, ConfirmationResult, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
-import { E164Number } from 'libphonenumber-js';
+import {
+  Auth,
+  ConfirmationResult,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import { E164Number } from "libphonenumber-js";
 
-const setUpRecaptcha = (phone: E164Number, auth: Auth): Promise<ConfirmationResult> => {
-    console.log('phone before', phone);
-    console.log('auth before', auth);
-
-  const recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
-    size: 'invisible',
+const setUpRecaptcha = (
+  phone: E164Number,
+  auth: Auth
+): Promise<ConfirmationResult> => {
+  const recaptchaVerifier = new RecaptchaVerifier(auth, "sign-in-button", {
+    size: "invisible",
     callback: (response: unknown) => {
-      // reCAPTCHA solved, allow signInWithPhoneNumber.
-      console.log('response invisible recaptchaVerifier', response);
+      console.log("response invisible recaptchaVerifier", response);
     },
   });
-  console.log('recaptchaVerifier after', recaptchaVerifier);
 
   return signInWithPhoneNumber(auth, `+${phone}`, recaptchaVerifier);
 };
